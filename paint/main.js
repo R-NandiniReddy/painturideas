7d5c8041190775d0c56799984789e4d5849f849f
R-NandiniReddy <144028249+R-NandiniReddy@users.noreply.github.com>
2023-09-14 09:45:02 -0400

    Add files via upload

---
+++ paint/main.js
@@ -0,0 +1,10 @@
+      function changeColor(color) {
+            var frame = document.getElementById('frame');
+            frame.style.borderColor = color;
+            frame.style.backgroundColor = color;
+        }
+        function changeFrame(borderColor) {
+            var frame = document.getElementById('frame');
+              frame.style.borderColor = borderColor;
+        }
+    
